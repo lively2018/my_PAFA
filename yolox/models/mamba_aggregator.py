7c8abe3f6057545eb1984db16830bf9b216d25e3
Kyungsoon Song <kaebbi@gmail.com>
2026-02-12 16:19:40 +0900

    remove debug message and replac type with type_k in mamba_aggregator

--- yolox/models/mamba_aggregator.py
+++ yolox/models/mamba_aggregator.py
@@ -39,13 +39,13 @@ class MambaAggregator(nn.Module):
         self.memory_bank_p5 = MemoryBank(**memory_cfg)
 
 
-    def forward(self, x, ref_x, type):
+    def forward(self, x, ref_x, type_k):
         #kssong
-        if type == 0:
+        if type_k == 0:
             ref_x = self.memory_bank_p3.sample()
-        elif type == 1:
+        elif type_k == 1:
             ref_x = self.memory_bank_p4.sample()
-        elif type == 2:
+        elif type_k == 2:
             ref_x = self.memory_bank_p5.sample()
         #logger.info(f"ref_x shape: {ref_x.shape} x shape: {x.shape}")
         #print(f"After sampling: {gpu_mem_usage():.0f}")
@@ -58,32 +58,32 @@ class MambaAggregator(nn.Module):
         return aggregated_x
 
 
-    def reset_memory_bank(self, type):
+    def reset_memory_bank(self, type_k):
         #logger.info("reset_memory_bank")
-        if type == 0:
+        if type_k == 0:
             self.memory_bank_p3.reset()
-        elif type == 1:
+        elif type_k == 1:
             self.memory_bank_p4.reset()
-        elif type == 2:
+        elif type_k == 2:
             self.memory_bank_p5.reset()
 
-    def update_memory_bank(self, x, type):
+    def update_memory_bank(self, x, type_k):
         #logger.info("update_memory_bank")
-        if type == 0:
+        if type_k == 0:
             self.memory_bank_p3.update(x)
-        elif type == 1:
+        elif type_k == 1:
             self.memory_bank_p4.update(x)
-        elif type == 2:
+        elif type_k == 2:
             self.memory_bank_p5.update(x)
 
-    def init_memory_bank(self, x, type):
+    def init_memory_bank(self, x, type_k):
         #logger.info("init_memory_bank")
         #logger.info("x.shape: {}".format(x.shape))
-        if type == 0:
+        if type_k == 0:
             self.memory_bank_p3.init_memory(x)
-        elif type == 1:
+        elif type_k == 1:
             self.memory_bank_p4.init_memory(x)
-        elif type == 2:
+        elif type_k == 2:
             self.memory_bank_p5.init_memory(x)
 
     def forward_with_ref_x(self, x, ref_x):
