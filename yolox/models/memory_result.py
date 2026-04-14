13d4a00b326c3d8d007f665b4280cec288d883b6
Kyungsoon Song <kaebbi@gmail.com>
2026-04-14 11:19:08 +0900

    add memory to save result

---
+++ yolox/models/memory_result.py
@@ -0,0 +1,81 @@
+from loguru import logger
+import torch
+import torch.nn as nn
+from yolox.data.datasets.vid_classes import VID_classes
+#kssong
+#from mmcv.runner import BaseModule
+# from ..aggregators.selsa_aggregator import SelsaAggregator
+
+class MemoryResult(nn.Module):
+    #def __init__(self,
+    #             max_length=20000, key_length=2000,
+    #             sampling_policy='random', updating_policy='random',
+    #             ):
+    def __init__(self,
+                 max_length=4800,
+                 ):
+        super().__init__()
+        #kssong
+        self.max_length = max_length
+        self.update_memory_result_policy = 'age'
+        self.results = []
+        self.age = 0
+
+
+    def reset(self):
+        #kssong
+        if self.results is not None:
+            self.results.clear()  # Clear the list to free memory
+        self.age = 0
+        self.results = []
+        logger.info(f"reset_memory_result, memory result size: {len(self.results)}")
+
+    def init_memory_result_info(self, result_info, batch_set, batch_item):
+
+        #kssong
+        logger.info(f"init_memory_result_info, batch_set: {batch_set}, batch_item: {batch_item}, len(result_info): {len(result_info)}")
+        if result_info is not None:
+            self.results.extend([[(batch_set-1), batch_item, result_info_item] for result_info_item in result_info])
+
+    def update_memory_result_info(self, result_info, batch_set, batch_item):
+        #kssong
+        logger.info(f"update_memory_result_info, batch_set: {batch_set}, batch_item: {batch_item}, len(result_info): {len(result_info)}")
+        if result_info is not None:
+            self.results.extend([[(batch_set-1), batch_item, result_info_item] for result_info_item in result_info])
+
+    def init_memory_result(self, result):
+        """
+        init memory result
+        Args:
+            result_info: list of tensors
+
+        Returns:
+
+        """
+
+        logger.info(f"init_memory_result, len(result): {len(result)}")
+        for result_item in result:
+            logger.info(f"result_item: {result_item}")
+            self.results.append(result_item)
+            self.age += 1
+
+
+    def update_memory_result(self, new_result):
+
+        if len(self.results) < self.max_length:
+            for result_item in new_result:
+                logger.info(f"result_item: {result_item}")
+                self.results.append(result_item)
+                self.age += 1
+
+        elif self.updating_policy == "age":
+            for result_item in new_result:
+                logger.info(f"result_item: {result_item}")
+                self.results.pop(0)
+                self.results.append(result_item)
+                self.age += 1
+        else:
+            raise NotImplementedError("not implemented")
+
+    def __len__(self):
+        return len(self.results)
