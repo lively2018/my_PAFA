1e780dbf749c2ed91308befa7936f81e402d2ed6
Kyungsoon Song <kaebbi@gmail.com>
2026-04-14 15:14:37 +0900

    add m_conf option for P3, P4, and P5

--- tools/vid_eval.py
+++ tools/vid_eval.py
@@ -107,7 +107,8 @@ def make_parser():
     parser.add_argument('--tnum', default=-1, help='vid test sequences')
     parser.add_argument('--formal', default=False, action="store_true",help='vid test sequences')
     parser.add_argument('--nms', default=0.5, type=float,help='final nms threshold')
-    parser.add_argument('--m_conf', default=0, type=float,help='select reference features minimum conf score')
+    parser.add_argument('--m_conf', default=[0, 0, 0], type=float, nargs=3, metavar=('P3', 'P4', 'P5'),
+                        help='per-level minimum conf score for selecting reference features (P3 P4 P5)')
     parser.add_argument('--memory_length', default=4800, type=int, help='memory length for Mamba aggregator')
     parser.add_argument('--key_length', default=480, type=int, help='key length for Mamba aggregator')
 
